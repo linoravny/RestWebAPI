fc37bf9a3562a46d73e62d85a93970465f625c54
Lin <linoravny@users.noreply.github.com>
2020-02-06 15:41:07 +0200

    work with map

--- DesktopApp/app.js
+++ DesktopApp/app.js
@@ -3,16 +3,19 @@ function getUsers() {
     const tblContainer = document.getElementById("tbl_container");
 
     var request = new XMLHttpRequest();//JSONHttpRequest
-    request.open('GET', 'http://localhost:50731/api/Users/getUsers', true);
+    request.open('GET', 'http://localhost:50731/api/Users/getUsers', true); //WEB API C#
+    //request.open('GET', 'http://localhost:3000/getUsers', true); //WEB API NodeJS
+
     request.responseType = 'json';
     //request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
    // request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    var arrOfUsersFileds = ["first_name", "last_name", "email", "country"]
+    //request.setRequestHeader('Access-Control-Allow-Origin', '*');
+    var arrOfUsersFileds = ["first_name", "last_name", "email"]
     request.onload = function () {
 
         var data = JSON.parse(this.response);
         if (data && request.status >= 200 && request.status < 400) {
-            data.forEach(usr => {
+            data.map(usr => {
                 const tr = document.createElement('tr');
                 tr.setAttribute('class', 'userTr');
                 tblContainer.appendChild(tr);
@@ -20,7 +23,7 @@ function getUsers() {
                 var td;
                 var text;
 
-                arrOfUsersFileds.forEach(key => {
+                arrOfUsersFileds.map((key, index) => {
                     td = document.createElement('td');
                     text = usr[key];
                     td.innerText = text;
